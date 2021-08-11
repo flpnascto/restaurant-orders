67bece49ca06d77fc8972b869f660176dfcb7747
Felipe Nascimento <flpnascto@gmail.com>
2021-08-11 11:50:18 -0300

    requisito 03

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,32 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.inventory = dict(self.MINIMUM_INVENTORY)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        for ingredient in InventoryControl.INGREDIENTS[order]:
+            if self.inventory[ingredient] > 0:
+                self.inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        quantities_to_buy = {
+            ingredient: self.MINIMUM_INVENTORY[ingredient]
+            - self.inventory[ingredient]
+            for ingredient in self.inventory.keys()
+        }
+        return quantities_to_buy
